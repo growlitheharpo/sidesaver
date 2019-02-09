93ae52d5cb47a5b798f7a295af9ad04825553a63
James Keats <growlitheharpo@gmail.com>
2019-02-09 11:59:57 -0500

    Setup SideSaver as a singleton and use it as a data context

--- src/SideSaver.cs
+++ src/SideSaver.cs
@@ -1,9 +1,29 @@
-﻿namespace sidesaver
+﻿using System.Windows;
+
+namespace sidesaver
 {
 	public class SideSaver
 	{
+		public static SideSaver instance { get; private set; }
+
+		public string MainScreenText
+		{
+			get { return "This is some stuff!"; }
+		}
+
 		[System.STAThread]
 		public static void Main()
+		{
+			SideSaver s = new SideSaver();
+		}
+
+		private SideSaver()
+		{
+			instance = this;
+			Initialize();
+		}
+
+		private void Initialize()
 		{
 			App app = new App();
 			app.InitializeComponent();
